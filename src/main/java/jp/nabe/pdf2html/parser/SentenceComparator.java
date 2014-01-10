b12d88b42a31c541a0eed39568f5e2db767e96ad
nabe <m.nabe.8559@gmail.com>
2014-01-10 16:58:53 +0900

    fix comparator
    lowered the priority of uncompleted sentence

--- src/main/java/jp/nabe/pdf2html/parser/SentenceComparator.java
+++ src/main/java/jp/nabe/pdf2html/parser/SentenceComparator.java
@@ -1,6 +1,10 @@
 package jp.nabe.pdf2html.parser;
 
 import java.util.Comparator;
+import java.util.LinkedList;
+
+import org.atilika.kuromoji.Token;
+import org.atilika.kuromoji.Tokenizer;
 
 public class SentenceComparator implements Comparator<Sentence> {
 
@@ -18,12 +22,12 @@ public class SentenceComparator implements Comparator<Sentence> {
             return 0;
         }
 
-        if (isShortSentence(s1)) {
-            if (!isShortSentence(s2)) {
-                return 1;
+        if (isSentenceCompleted(s1)) {
+            if (!isSentenceCompleted(s2)) {
+                return -1;
             }
-        } else if (isShortSentence(s2)) {
-            return -1;
+        } else if (isSentenceCompleted(s2)) {
+            return 1;
         }
 
         if (s1.near(s2)) {
@@ -39,12 +43,87 @@ public class SentenceComparator implements Comparator<Sentence> {
         return s1.compareTo(s2);
     }
 
-    protected boolean isShortSentence(Sentence sentence) {
-        return sentence.getCharLength() < SentenceProperty.SHORT_LENGTH;
-    }
-
     protected boolean isLargeFontSentence(Sentence sentence) {
         return sentence.getFontSize() > SentenceProperty.LARGE_FONT;
+    }
+
+    protected boolean isSentenceCompleted(Sentence sentence) {
+        if (sentence.getCharLength() < SentenceProperty.SHORT_LENGTH) {
+            return false;
+        }
+
+        Tokenizer tokenizer = Tokenizer.builder().build();
+        LinkedList<Token> tokens = new LinkedList<Token>(tokenizer.tokenize(sentence.getValue()));
+
+        Token first = tokens.getFirst();
+        String firstFeatures = first.getAllFeatures();
+        if (!isFirstWord(firstFeatures)) {
+            return false;
+        }
+
+        Token last = tokens.getLast();
+        if (first.equals(last)) {
+            return false;
+        }
+        String lastFeatures = last.getAllFeatures();
+        if (!isLastWord(lastFeatures)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected boolean isFirstWord(String features) {
+        if (features.startsWith("形容詞")) {
+            return true;
+        }
+        if (features.startsWith("名詞")) {
+            return true;
+        }
+        if (features.startsWith("接続詞")) {
+            return true;
+        }
+        if (features.startsWith("連体詞")) {
+            return true;
+        }
+        if (features.startsWith("副詞")) {
+            return true;
+        }
+        if (features.startsWith("接頭詞")) {
+            return true;
+        }
+        if (features.startsWith("感動詞")) {
+            return true;
+        }
+        if (features.startsWith("記号,アルファベット")) {
+            return true;
+        }
+        if (features.startsWith("記号,括弧開")) {
+            return true;
+        }
+        if (features.startsWith("記号,一般")) {
+            return true;
+        }
+        return false;
+    }
+
+    protected boolean isLastWord(String features) {
+        if (features.startsWith("記号,句点")) {
+            return true;
+        }
+        if (features.startsWith("記号,一般")) {
+            return true;
+        }
+        if (features.startsWith("記号,括弧閉")) {
+            return true;
+        }
+        if (features.startsWith("動詞")) {
+            return true;
+        }
+        if (features.startsWith("助動詞")) {
+            return true;
+        }
+        return false;
     }
 
 }
