6fa818458ce5653a6b9f417a22d54df0adada019
kenta.watanabe <m.nabe.8559@gmail.com>
2014-01-11 09:37:38 +0900

    create sentence parser

--- src/main/java/jp/nabe/pdf2html/parser/SentenceComparator.java
+++ src/main/java/jp/nabe/pdf2html/parser/SentenceComparator.java
@@ -1,10 +1,6 @@
 package jp.nabe.pdf2html.parser;
 
 import java.util.Comparator;
-import java.util.LinkedList;
-
-import org.atilika.kuromoji.Token;
-import org.atilika.kuromoji.Tokenizer;
 
 public class SentenceComparator implements Comparator<Sentence> {
 
@@ -52,78 +48,8 @@ public class SentenceComparator implements Comparator<Sentence> {
             return false;
         }
 
-        Tokenizer tokenizer = Tokenizer.builder().build();
-        LinkedList<Token> tokens = new LinkedList<Token>(tokenizer.tokenize(sentence.getValue()));
-
-        Token first = tokens.getFirst();
-        String firstFeatures = first.getAllFeatures();
-        if (!isFirstWord(firstFeatures)) {
-            return false;
-        }
-
-        Token last = tokens.getLast();
-        if (first.equals(last)) {
-            return false;
-        }
-        String lastFeatures = last.getAllFeatures();
-        if (!isLastWord(lastFeatures)) {
-            return false;
-        }
-
-        return true;
-    }
-
-    protected boolean isFirstWord(String features) {
-        if (features.startsWith("形容詞")) {
-            return true;
-        }
-        if (features.startsWith("名詞")) {
-            return true;
-        }
-        if (features.startsWith("接続詞")) {
-            return true;
-        }
-        if (features.startsWith("連体詞")) {
-            return true;
-        }
-        if (features.startsWith("副詞")) {
-            return true;
-        }
-        if (features.startsWith("接頭詞")) {
-            return true;
-        }
-        if (features.startsWith("感動詞")) {
-            return true;
-        }
-        if (features.startsWith("記号,アルファベット")) {
-            return true;
-        }
-        if (features.startsWith("記号,括弧開")) {
-            return true;
-        }
-        if (features.startsWith("記号,一般")) {
-            return true;
-        }
-        return false;
-    }
-
-    protected boolean isLastWord(String features) {
-        if (features.startsWith("記号,句点")) {
-            return true;
-        }
-        if (features.startsWith("記号,一般")) {
-            return true;
-        }
-        if (features.startsWith("記号,括弧閉")) {
-            return true;
-        }
-        if (features.startsWith("動詞")) {
-            return true;
-        }
-        if (features.startsWith("助動詞")) {
-            return true;
-        }
-        return false;
+        SentenceParser parser = new SentenceParser();
+        return parser.isSentenceCompleted(sentence);
     }
 
 }
